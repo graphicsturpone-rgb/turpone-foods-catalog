39122986a0137a7769e9c94c094e071129331261
graphicsturpone-rgb <graphicsturpone@gmail.com>
2026-09-17 10:59:47 -0400

    chore: clean up all leftover patch scripts, backup files, and deeply duplicated image folders

--- update-build.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-let content = fs.readFileSync('scripts/build-recipes.js', 'utf8');
-
-content = content.replace(/<div style="margin-top: 40px; text-align: center;">/g, '<div style="margin-top: 40px; text-align: left;">');
-
-fs.writeFileSync('scripts/build-recipes.js', content, 'utf8');
-console.log('Script updated successfully.');
